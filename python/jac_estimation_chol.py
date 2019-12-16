4b1cf7a7f5b630d760d0ceb730e7b35766379cbf
tobiasstenzel <tobias.stenzel@mailbox.org>
2019-12-16 18:54:06 +0100

    joined latex and some programs

---
+++ python/jac_estimation_chol.py
@@ -0,0 +1,175 @@
+"""
+Estimates covariance matrix for KW94 Dataset 1 with Simulated Max. Likelihood.
+
+"""
+
+import json
+
+import numpy as np
+import pandas as pd
+import respy as rp
+
+from estimagic.inference.likelihood_covs import cov_jacobian
+from estimagic.differentiation.differentiation import jacobian
+from estimagic.optimization.optimize import maximize
+from respy.likelihood import get_crit_func
+
+
+def jac_estimation(save=False):
+    """
+    Estimates covariance matrix for KW94 Dataset 1 with Simulated Max. Likelihood.
+    The Jacobian matrix is used instead of Hessian because its much faster.
+    The parameters contain Cholesky factors instead of SD-Corr-Matrix because
+    these factors are unconstrained. Therefore, their distribution can be estimated
+    by an unconstrained normal distribution.
+
+    Parameters
+    ----------
+    save: Bool
+        Indicates wether to save data.
+    
+    Returns
+    -------
+    par_df: DataFrame
+        Df containing parameters, SDs and lower and upper bound in estimagic format.
+    cov_df: DataFrame
+        Df containing the covariance matrix.
+    corr_df: DataFrame
+        DF containing the correlation matrix.
+
+    """
+    # Df is sample of 1000 agents in 40 periods.
+    params, options, df = rp.get_example_model("kw_94_one")
+
+    # Write params in terms of Cholesky factors instead of SD-Corr-matrix.
+    # This transformation holds only true for the parametrization in KW94 Dataset 1.
+    # Simply change SD-Corr indices to cholesky indices.
+    params_chol = _chol_indexed_params(params)
+
+    # Estimate parameters.
+    # log_like = log_like_obs.mean(). Used for consistency with optimizers.
+    # Gives log-likelihood function for mean agent.
+    crit_func = rp.get_crit_func(params_chol, options, df, 'log_like')
+
+    # Get constraint for parameter estimation
+    constr = rp.get_parameter_constraints('kw_94_one')
+    # Kick out constraints for SD-Corr-Matrix. Cholesky factors are unconstrained.
+    constr_chol = constr[1:4]
+
+    # Estimate parameters
+    _, par_estimates = maximize(
+    crit_func,
+    params_chol,
+    'scipy_L-BFGS-B',
+    db_options={'rollover': 200},
+    algo_options={'maxfun': 1},
+    constraints=constr_chol,
+    dashboard=False
+    )
+
+    # df  will take lower and upper bounds after standard error esitmation
+    # so that cols fit topography plot requirements.
+    par_df = pd.DataFrame(
+        data=par_estimates['value'].values[:27],
+        index=params_chol[:27].index,
+        columns=['value']
+    )
+
+    # The rest of this function estimates the variation of the estimates.
+    # Log-likelihood function for sample of agents.
+    log_like_obs_func = get_crit_func(params_chol, options, df, version='log_like_obs')
+
+    # Jacobian matrix.
+    jacobian_matrix = jacobian(log_like_obs_func, params_chol, extrapolation=False)
+
+    # Drop zero lines to avoid multicollinearity for matrix inversion.
+    jacobian_matrix = jacobian_matrix.loc[:, (jacobian_matrix != 0).any(axis=0)]
+
+    jacobian_cov_matrix = cov_jacobian(jacobian_matrix.to_numpy())
+
+    jacobian_cov_matrix = cov_jacobian(jacobian_matrix.to_numpy())
+
+    cov_df = pd.DataFrame(
+    data=jacobian_cov_matrix,
+    index=params_chol[:27].index,
+    columns=params_chol[:27].index,
+    )
+
+    corr_df = cov_df.copy(deep=True)
+    for i in range(0,len(cov_df)):
+        for j in range(0,len(cov_df)):
+            corr_df.iloc[i,j] = cov_df.iloc[i,j]/(
+                np.sqrt(cov_df.iloc[i,i]*cov_df.iloc[j,j]))
+
+    assert -1 <= corr_df.values.any() <= 1, "Corrs must be inside [-1,1]"
+
+    # Estimate parameters.
+    # log_like = log_like_obs.mean(). Used for consistency with optimizers.
+    # Gives log-likelihood function for mean agent.
+    crit_func = rp.get_crit_func(params_chol, options, df, 'log_like')
+
+    constr = rp.get_parameter_constraints('kw_94_one')
+    # kick out constraints for SD-Corr-Matrix. Cholesky factors are unconstrained.
+    constr_chol = constr[1:4]
+
+    _, par_estimates = maximize(
+    crit_func,
+    params_chol,
+    'scipy_L-BFGS-B',
+    db_options={'rollover': 200},
+    algo_options={'maxfun': 1},
+    constraints=constr_chol,
+    dashboard=False
+    )
+
+    # Include upper and lower bounds to par_df for topography plot.
+    par_df['sd'] = np.sqrt(np.diag(jacobian_cov_matrix))
+    par_df['lower'] = par_df['value'] - 2* par_df['sd']
+    par_df['upper'] = par_df['value'] + 2* par_df['sd']
+
+    if save is True:
+        cov_df.to_pickle("input/cov.uq.pkl")
+        corr_df.to_pickle("input/corr.uq.pkl")
+        par_df.to_pickle("input/params.uq.pkl")
+        # contains 3 fixed respy params
+        params_chol.to_pickle("input/base_params.uq.pkl")
+    else:
+        pass
+  
+    return par_df, cov_df, corr_df
+
+
+def _chol_indexed_params(params):
+    """ Creates the params Df with Cholesky factors and the right indices for respy.
+    This transformation holds only true for the parametrization in KW94 Dataset 1.
+    It simply changes SD-Corr indices to cholesky indices.
+    Without the slicing and merging, index ('maximum_exp', 'edu') yields
+    an uniqueness error for the second index when (..., 'sd_edu') is set to (..., 'edu').
+    Yet, since we have double_indices the indices ARE unique.
+
+    """
+    p_chol_slice = params.iloc[17:27,:]
+    # remove unused inherited index levels
+    p_chol_slice.index = p_chol_slice.index.remove_unused_levels()
+    # Use the SPECIFIC property of Dataset 1 in KW94 where SD-Corr-Matrix equals Cholesky maxtrix
+    # This mean we just need to, firstly, rename the first index.
+    p_chol_slice.index = p_chol_slice.index.set_levels(p_chol_slice.index.levels[0].str.replace('shocks_sdcorr', 'shocks_chol'), level=0)
+
+    # And secondly we need to convert the second index to respy cholesky format.
+    dic = { "sd": "chol", "corr": "chol"}
+    for i, j in dic.items():
+        p_chol_slice.index = p_chol_slice.index.set_levels(p_chol_slice.index.levels[1].str.replace(i, j), level=1)
+
+    # insert params_chol with index in params by merging slices.
+    part_1 = params.iloc[0:17,:]
+    part_1.index = part_1.index.remove_unused_levels()
+    part_3 = params.iloc[27:31,:]
+    part_3.index = part_3.index.remove_unused_levels()
+
+    parts = [part_1, p_chol_slice, part_3]
+    params_chol = pd.concat(parts)
+
+    return params_chol
+
+# Call function.
+jac_estimation(save=True)
---
+++ python/plots.py
@@ -0,0 +1,202 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+#plt.style.use('mplstyle/uq.mplstyle')
+
+
+def heatmap_corr_chol(corr_df, save=False):
+    select = corr_df.iloc[[0,1,7,13,16,19,20],[0,1,7,13,16,19,20]]
+
+    name="corrs"
+    labels = np.array([r'$\delta$', r'$\beta^{b}$',  r'$\beta_{e}^{b}$',
+             r'$\beta_{b}^{b}$', r'$\beta_{bb}^{b}$', r'$\beta_{w}^{b}$', 
+             r'$\beta_{ww}^{b}$', r'$\beta^{w}$', r'$\beta_{e}^{w}$', 
+             r'$\beta_{w}^{w}$', r'$\beta_{ww}^{w}$', r'$\beta_{b}^{w}$',
+             r'$\beta_{bb}^{w}$', r'$\beta^{e}$', r'$\beta_{col}^{e}$', r'$\beta_{re}^{e}$',
+             r'$\beta^{h}$', r'$c_{b}$', r'$c_{w}$', r'$c_{e}$',
+             r'$c_{h}$', r'$c_{b,w}$', r'$c_{b3}$', r'$c_{w,e}$', 
+             r'$c_{b,h}$', r'$c_{w,h}$',  r'$c_{e.h}$'])
+
+    fig = plt.figure(figsize=(15,10))
+
+    mask = np.zeros_like(select)
+    mask[np.triu_indices_from(select, 1)] = True
+
+    # Aspects for heigth, pad for whitespace
+    ax = sns.heatmap(
+             np.round(select, 2), mask=mask,
+             cmap="RdBu_r", linewidths=0.0, square=False, vmin=-1, vmax=1, annot=True)
+             #cbar_kws = dict(
+                #use_gridspec=True, location="right", aspect=25, pad=0.05,
+                #shrink=0.965))
+    ax.tick_params(axis='both', direction='out', length=6, width=2)
+    ax.set_yticklabels(labels[[0,1,7,13,16,19,20]], ha = 'left', rotation=0);
+    ax.set_xticklabels(labels[[0,1,7,13,16,19,20]], rotation=0);
+    ax.set_ylabel(r'$\hat{\theta}$', labelpad=+35, rotation=0)
+    ax.set_xlabel(r'$\hat{\theta}$',labelpad=+20)
+
+    cbar = ax.collections[0].colorbar
+
+    # Positioning at -1 needs vmin.
+    cbar.set_ticks([-1, -0.5, 0, 0.5, 1])
+    cbar.set_ticklabels(['-1.0', ' -0.5', '0.0', '0.5', '1.0'])
+    cbar.ax.tick_params(direction='out', length=6, width=2)
+    
+    #a bit more space for xlabels
+    ax.tick_params(axis='x', which='major', pad=8)
+
+    #left-align y labels
+    yax = ax.get_yaxis()
+    pad = max(T.label.get_window_extent().width for T in yax.majorTicks) +5
+    yax.set_tick_params(pad=pad)
+
+    if save is True:
+        plt.savefig("figures/heatmap.png", bbox_inches="tight")
+    else:
+        pass
+    
+    return fig, ax
+
+
+def distplot(sample, qoi_name, save=False):
+    """
+    This function is a custom-made wrapper around seaborn.distplot.
+
+    Parameters
+    ----------
+    sample: Series, 1d-array, or list.
+        A vector of random observations.
+    qoi_name: str
+        Name of Quantity of interest used for x label and png-name.
+    save: bool
+        Indicate whether to save the plot as png.
+
+    Returns
+    -------
+    dp: Figure
+        Returns Figure object setting figure-level attributes.
+
+    """
+    fig, ax = plt.subplots()
+
+    # Plot mean as vertical line.
+    mean = ax.axvline(
+        np.mean(sample), color="#1245A8", linestyle="--", lw=4, label="Sample Mean"
+    )
+
+    # Call seaborn.distplot and set options.
+    dp = sns.distplot(
+        sample,
+        hist=True,
+        kde=True,
+        bins=100,
+        norm_hist=True,
+        hist_kws=dict(alpha=0.4, color="#1245A8", edgecolor="#1245A8"),
+        kde_kws=dict(color="#1245A8", linewidth=5),
+    )
+
+    ax.set_title("Distribution of {}".format(qoi_name), y=1.05)
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.set_ylabel("Kernel Density Estimate", labelpad=30)
+    ax.legend(handles=[mean], edgecolor="white")
+
+    if save is True:
+        plt.savefig("figures/distplot_{}.png".format(qoi_name), bbox_inches="tight")
+    else:
+        pass
+
+    return dp, ax
+
+
+def convergence_plot(sample, expected, qoi_name, absolute_deviation=False, save=False):
+    """
+    This function is a custom-made convergence plot for some Monte-Carlo
+    sample.
+
+    Parameters
+    ----------
+    sample: Series, 1d-array, or list.
+        A vector of random observations.
+    expected: float, int.
+        Expected value of sample mean.
+    qoi_name: str
+        Label of y-axis.
+    absolute_deviation: bool
+        Plots absolute deviation of means to zero expectation value.
+    save: bool
+        Indicates whether to save the plot as png.
+
+    Returns
+    -------
+    dp: Figure
+        Returns Figure object setting figure-level attributes.
+
+    """
+    df = pd.DataFrame(sample, columns=["qoi_realization"])
+    df["cum_sum"] = df["qoi_realization"].cumsum()
+    df["mean_iteration"] = df["cum_sum"] / (df.index.to_series() + 1)
+
+    fig, ax = plt.subplots()
+
+    if absolute_deviation is not True:
+        # Compute sample mean for each iteration
+        title = "Convergence of Monte-Carlo Uncertainty Propagation (level)"
+        file_str = "level"
+        legend_loc = "lower right"
+
+        conv_plot, = ax.plot(
+            df.index + 1,
+            df["mean_iteration"],
+            color="#1245A8",
+            lw=3.0,
+            label="Sample Mean",
+        )
+
+    else:
+        title = "Convergence of MC Uncertainty Propagation (absolute deviation)"
+        file_str = "abs_dev"
+        legend_loc = "upper right"
+
+        conv_plot, = ax.plot(
+            df.index + 1,
+            abs(df["mean_iteration"] - expected),
+            color="#1245A8",
+            lw=3.0,
+            label="Sample Mean",
+        )
+        expected = 0
+
+    # Plot expected value.
+    exp_plot = ax.hlines(
+        expected,
+        1,
+        len(sample),
+        lw=2.5,
+        linestyle="--",
+        label="Under Mean Parametrization",
+    )
+
+    ax.set_title(title, y=1.05)
+    ax.set_xlim(1, len(sample))
+    ax.grid(True, linestyle=(0, (5, 10)))
+    ax.set_ylabel(qoi_name, labelpad=14)
+    ax.set_xlabel("Number of iterations", labelpad=14)
+    ax.tick_params(axis="both")
+    ax.legend(
+        handles=[exp_plot, conv_plot], loc=legend_loc, edgecolor="black", fancybox=False
+    )
+
+    if save is True:
+        plt.savefig(
+            "figures/convergence_plot_{}_{}.png".format(file_str, qoi_name),
+            bbox_inches="tight",
+        )
+    else:
+        pass
+
+    return plt, ax
