189177bec6a707182e7df731bae47df6f633e961
tobiasstenzel <tobias.stenzel@mailbox.org>
2020-01-08 19:53:39 +0100

    save

--- python/morris_screening.py
+++ python/morris_screening.py
@@ -4,12 +4,19 @@ import numpy as np
 
 n_inputs = 2
 n_levels = 4
-stepsize = n_levels / (2 * (n_levels - 1))
+
+
+def stepsize(n_levels):
+    """Leads to relatively equiprobable sampling from the input distribution"""
+    return n_levels / (2 * (n_levels - 1))
 
 
 def morris_trajectories(
-    init_input_pars, n_inputs, stepsize, seed=123, test_D_star_rand_2dim=False
-):
+        init_input_pars, stepsize, seed=123, test_D_star_rand_2dim=False
+        ):
+    """Returns n parameter vectors, Dim n x Theta.
+    n is also Theta+1."""
+    n_inputs = len(init_input_pars)
     np.random.seed(seed)
     #  B is (p+1)*p strictily lower triangular matrix of ones.
     B = np.tril(np.ones([n_inputs + 1, n_inputs]), -1)
@@ -45,7 +52,69 @@ def elementary_effect_i(model, i_python, init_input_pars, stepsize):
 
 def scaled_elementary_effect_i(
     model, i_python, init_input_pars, stepsize, sd_i, sd_model
-):
+    ):
+    """Scales EE by (SD_i / SD_M)"""
     ee_i = elementary_effect_i(model, i_python, init_input_pars, stepsize)
 
     return ee_i * (sd_i / sd_model)
+
+def sobol_model(input_pars, coeffs_a):
+    """
+    Arguments are lists. Strongly nonlinear, nonmonotonic, and nonzero interactions.
+    Analytic results for Sobol Indices.
+    """
+    def g_i(input_par_i, coeffs_a_i):
+        return (abs(4 * input_par_i - 2) + coeffs_a_i) / (1 + coeffs_a_i)
+    y = 1
+    for i in range(0, len(input_pars)):
+        y *= g_i(input_pars[i], coeffs_a[i])
+    return y
+
+
+# afterwards implement this for all elements of a traj list.
+
+
+
+def compute_trajectory_distance(traj_0, traj_1):
+    """
+    Computes the sum of the root of the square distance between each
+    parameter vector of one trajectory to each vector of the other trajectory.
+    Trajectories are np.Arrays with iterations as rows and parameters as vectors.
+    
+    """
+    distance = 0
+    
+    assert np.size(traj_0, 0) == np.size(traj_0, 1) + 1
+    assert traj_0.shape == traj_1.shape
+    if np.any(np.not_equal(traj_0, traj_1)):
+        for col_0 in range(0,  np.size(traj_0, 1)):
+            for col_1 in range(0,  np.size(traj_1, 1)):
+                distance += np.sqrt(sum((traj_0[:,col_0] - traj_1[:,col_1])**2))
+    else:
+        pass
+    
+    return distance
+
+# afterwards implement this for all elements of a traj list.
+def campolongo_2007(trajectory_list):
+    distance_matrix = np.nan * np.ones(shape=(len(trajectory_list),len(trajectory_list)))
+    for i in range(0, len(trajectory_list)):
+        for j in range(0, len(trajectory_list)):
+            distance_matrix[i,j] = compute_trajectory_distance(
+                    trajectory_list[i],
+                    trajectory_list[j])
+            return distance_matrix
+
+traj_0 = np.ones((3,2))
+traj_1 = np.zeros((3,2))
+
+compute_trajectory_distance(traj_0, traj_1)
+campolongo_2007([traj_0, traj_1])
+    
+    
+    
+
+
+
+
+
--- python/tests/test_morris_screening.py
+++ python/tests/test_morris_screening.py
@@ -8,17 +8,18 @@ import numpy as np
 from numpy.testing import assert_array_equal
 from morris_screening import morris_trajectories
 from morris_screening import elementary_effect_i
+from morris_screening import compute_trajectory_distance
+
 
 
 def test_morris_trajectories():
-    n_inputs = 2
-    init_input_pars = np.array([1 / 3, 1 / 3], ndmin=n_inputs)
+    init_input_pars = np.array([1 / 3, 1 / 3])
     stepsize = 2 / 3
     expected = np.array([[1, 1 / 3], [1, 1], [1 / 3, 1]])
     assert_array_equal(
         expected,
         morris_trajectories(
-            init_input_pars, n_inputs, stepsize, seed=123, test_D_star_rand_2dim=True
+            init_input_pars, stepsize, seed=123, test_D_star_rand_2dim=True
         ),
     )
 
@@ -35,3 +36,8 @@ def test_elemtary_effect_i():
     assert 1 == round(
         elementary_effect_i(lin_portfolio, 1, [0.5, 1], stepsize=2 / 3), 10
     )
+
+def test_compute_trajectory_distance():
+    traj_0 = np.ones((3,2))
+    traj_1 = np.zeros((3,2))
+    assert 4 * np.sqrt(3) == compute_trajectory_distance(traj_0, traj_1)
