c338293c042c84a031976c18e99000f81459211b
tobiasstenzel <tobias.stenzel@mailbox.org>
2020-01-20 01:51:39 +0100

    travis fix?

--- utils/travis_runner.py
+++ utils/travis_runner.py
@@ -4,7 +4,9 @@ import glob
 import subprocess as sp
 
 if __name__ == "__main__":
-    sp.check_call("python -m pytest python/tests/", shell=True)
+	sp.check_call("cd python/tests", shell=True)
+    sp.check_call("pytest", shell=True)
+    sp.check_call("cd ../..", shell=True)
     # Commented out to save travis around 5 minutes.
     # sp.check_call("python python/jac_estimation_chol.py", shell=True)
     sp.check_call("python python/script_uncertainty_propagation.py", shell=True)
