2c766517024c218cff19ec896b7f231ca97b495f
tobiasstenzel <tobias.stenzel@mailbox.org>
2020-01-21 16:48:46 +0100

    inverse nataf trans is tested for uncorrelated params

--- python/ge_menendez_2017_traj_transformation.py
+++ python/ge_menendez_2017_traj_transformation.py
@@ -1,16 +1,16 @@
 """Ge/Menendez (2017)"""
+import random
+
 import numpy as np
-
-from scipy.stats import norm
 import scipy.linalg as linalg
-
-from sampling_trajectory import morris_trajectory
 from sampling_trajectory import intermediate_ge_menendez_2014
+from sampling_trajectory import morris_trajectory
+from scipy.stats import norm
 
 
 # Transformation 1: Shift the first \omega elements to the back to generate
 # an independent vector.
-def transformation_one(traj, p_i_plus_one=False):
+def reorder_trajectory(traj, p_i_plus_one=False):
     traj_trans_one = np.ones([np.size(traj, 0), np.size(traj, 1)]) * np.nan
     for i in range(0, np.size(traj, 0)):
         # move FIRST w elements to the BACK
@@ -22,7 +22,7 @@ def transformation_one(traj, p_i_plus_one=False):
 
 
 # Transformation 3: Undo Transformation 1.
-def rev_transformation_one(traj, p_i_plus_one=False):
+def reverse_reorder_trajectory(traj, p_i_plus_one=False):
     traj_trans_three = np.ones([np.size(traj, 0), np.size(traj, 1)]) * np.nan
     for i in range(0, np.size(traj, 0)):
         # move LAST w elements to the FRONT
@@ -38,75 +38,88 @@ sample_traj_list = list()
 for traj in range(0, n_traj_sample):
     seed = 123 + traj
 
-    sample_traj_list.append(
-        morris_trajectory(n_inputs=3, n_levels=6)
-    )
-    
+    sample_traj_list.append(morris_trajectory(n_inputs=3, n_levels=6))
+
 gm14_array, gm14_list, gm14_pairs_dist = intermediate_ge_menendez_2014(
     sample_traj_list, n_traj=5
-    )
+)
 
 
 traj = sample_traj_list[0]
 
-traj_trans_one = transformation_one(traj)
-traj_trans_one_compare = transformation_one(traj, p_i_plus_one=True)
-traj_trans_rev = rev_transformation_one(traj_trans_one)
-traj_trans_rev_compare = rev_transformation_one(
+traj_trans_one = reorder_trajectory(traj)
+traj_trans_one_compare = reorder_trajectory(traj, p_i_plus_one=True)
+traj_trans_rev = reverse_reorder_trajectory(traj_trans_one)
+traj_trans_rev_compare = reverse_reorder_trajectory(
     traj_trans_one_compare, p_i_plus_one=True
 )
 
 
 """Try tranformation 2"""
 # Check in mu and cov.
-cov = np.array(
-          [[1, 0.9, 0.4],
-           [0.9, 1, 0.01],
-           [0.4, 0.01, 1]])
+cov = np.identity(3)
+cov_1 = np.array([[1, 0.9, 0.4], [0.9, 1, 0.01], [0.4, 0.01, 1]])
 mu = np.zeros(np.size(cov, 0))
 
-# Need to replace ones, because erfinv(1) = inf and zeros because erfinv(0) = -inf
-traj_trans_approx = np.where(traj_trans_one == 1, 0.999, traj_trans_one)
-traj_trans_approx = np.where(traj_trans_approx == 0, 0.001, traj_trans_approx)
 
-# Step 1: Inverse cdf of standard normal distribution (N(0, 5)).
-z = norm.ppf(traj_trans_approx)
-
-# Step 2. Skipped transformation of covariance matrix for normally distributed paramters.
-r_z = cov
+def sample_stnormal_paramters(n_par, n_draws=100_000, seed=123):
+    random.seed(seed)
+    sample_stnormal_paramters = np.random.normal(0, 1, n_par * n_draws).reshape(
+        n_par, n_draws
+    )
+    return sample_stnormal_paramters
 
 
-# Step 3: Perform Cholesky decoposition of (transformed) covariance matrix for
-# upper triangular matrix.
-n_par = np.size(r_z, 0)
-n_draws = 10000
-
-m = linalg.cholesky(r_z, lower=False)
+sample_Z_c = sample_stnormal_paramters(np.size(cov, 0))
 
 
-# Step 4: Draw random vector from standard normal distribution for each paramter
-# and calculate the corresponding correlation matrix. Then do the same as in
-# Step 3 for the correlation matrix.
-c_1 = np.corrcoef(r_z)
-
-sample_random_normal_paramters = np.random.normal(0, 1, n_par*n_draws).reshape(n_par, n_draws)
-
-c = np.corrcoef(sample_random_normal_paramters)
-
-q = linalg.cholesky(c, lower=False)
-
-# Step 5: Derive the dependent normally distributed vector z_c = z*c^(-1)*m.
-z_c = np.ones(traj.shape) * np.nan
-for row in range(0, n_par + 1):
-    zq = np.dot(z[row, :], linalg.inv(q))
-    z_c[row, :] = np.dot(zq, m)
-
-# Step 6: Apply inverse Nataf transformation.
-phi_z_c = norm.cdf(z_c)
-
-x = np.ones(traj.shape) * np.nan
-for row in range(0, n_par + 1):
-    x[row, :] = mu + np.dot(norm.ppf(phi_z_c)[row, :], np.sqrt(cov))
+row_traj_reordered = traj_trans_one[0, :]
 
 
+def inverse_nataf_transformation_normal(row_traj_reordered, mu, cov, sample_Z_c):
+    # Need to replace ones, because norm.ppf(1) = inf and zeros because norm.ppf(0) = -inf
+    row_approx = np.where(row_traj_reordered == 1, 0.999, row_traj_reordered)
+    row_approx = np.where(row_approx == 0, 0.001, row_approx)
 
+    # Step 1: Inverse cdf of standard normal distribution (N(0, 5)).
+    z = norm.ppf(row_approx)
+
+    # Step 2. Skipped transformation of covariance matrix for normally distributed paramters.
+    R_z = cov
+
+    # Step 3: Perform Cholesky decomposition of (transformed) covariance matrix for
+    # upper triangular matrix.
+    M = linalg.cholesky(R_z, lower=False)
+
+    # Step 4: Draw random vector from standard normal distribution for each paramter
+    # and calculate the corresponding correlation matrix. Then do the same as in
+    # Step 3 for the correlation matrix.
+    C = np.corrcoef(sample_Z_c)
+    Q = linalg.cholesky(C, lower=False)
+
+    # Step 5: Derive the dependent normally distributed vector z_c = z*Q^(-1)*M.
+    z_c = np.ones(row_traj_reordered.shape) * np.nan
+    zq = np.dot(z, linalg.inv(Q))
+    z_c = np.dot(zq, M)
+
+    # Step 6: Apply inverse Nataf transformation with Gaussian copula.
+    # Almost equal to z if there are no correlations.
+    cdf_z_c = norm.cdf(z_c)
+
+    row_x = np.ones(row_traj_reordered.shape) * np.nan
+    row_x = np.dot(mu + norm.ppf(cdf_z_c), np.sqrt(cov))
+
+    return row_x
+
+
+cov = np.identity(9)
+mu = np.zeros(9)
+
+row_traj_reordered = np.array([0, 0.01, 0.05, 0.2, 0.5, 0.8, 0.95, 0.99, 1])
+expected = np.array(
+    [-3.0902, -2.3263, -1.6449, -0.8416, 0.000, 0.8416, 1.6449, 2.3263, 3.0902]
+)
+
+sample_Z_c = sample_stnormal_paramters(np.size(cov, 0), n_draws=10_000_000)
+
+row_x = inverse_nataf_transformation_normal(row_traj_reordered, mu, cov, sample_Z_c)
--- python/tests/test_ge_menendez_2017_traj_transformation.py
+++ python/tests/test_ge_menendez_2017_traj_transformation.py
@@ -9,33 +9,52 @@ import numpy as np
 from numpy.testing import assert_array_equal
 from numpy.testing import assert_array_almost_equal
 
-from ge_menendez_2017_decorrelation import transformation_one
-from ge_menendez_2017_decorrelation import rev_transformation_one
+from ge_menendez_2017_traj_transformation import reorder_trajectory
+from ge_menendez_2017_traj_transformation import reverse_reorder_trajectory
+from ge_menendez_2017_traj_transformation import sample_stnormal_paramters
+from ge_menendez_2017_traj_transformation import inverse_nataf_transformation_normal
 
 
 def test_transformations():
     traj = np.array([[0, 0, 0], [1, 0, 0], [2, 3, 0], [4, 5, 6]])
 
     assert_array_equal(
-        transformation_one(traj, p_i_plus_one=True),
+        reorder_trajectory(traj, p_i_plus_one=True),
         np.array([[0, 0, 0], [0, 0, 1], [0, 2, 3], [4, 5, 6]]),
     )
 
     assert_array_equal(
         traj,
-        rev_transformation_one(
-            transformation_one(traj, p_i_plus_one=True), p_i_plus_one=True
+        reverse_reorder_trajectory(
+            reorder_trajectory(traj, p_i_plus_one=True), p_i_plus_one=True
         ),
     )
 
     assert_array_equal(
-        transformation_one(traj, p_i_plus_one=False),
+        reorder_trajectory(traj, p_i_plus_one=False),
         np.array([[0, 0, 0], [0, 1, 0], [2, 3, 0], [5, 6, 4]]),
     )
 
     assert_array_equal(
         traj,
-        rev_transformation_one(
-            transformation_one(traj, p_i_plus_one=False), p_i_plus_one=False
+        reverse_reorder_trajectory(
+            reorder_trajectory(traj, p_i_plus_one=False), p_i_plus_one=False
         ),
     )
+
+
+def test_inverse_nataf_transformation_normal_uncorrelated():
+    """Tests only a vector of uncorrelated paramters."""
+    cov = np.identity(9)
+    mu = np.zeros(9)
+
+    row_traj_reordered = np.array([0, 0.01, 0.05, 0.2, 0.5, 0.8, 0.95, 0.99, 1])
+    expected = np.array(
+        [-3.0902, -2.3263, -1.6449, -0.8416, 0.000, 0.8416, 1.6449, 2.3263, 3.0902]
+    )
+
+    sample_Z_c = sample_stnormal_paramters(np.size(cov, 0), n_draws=10_000_000)
+
+    row_x = inverse_nataf_transformation_normal(row_traj_reordered, mu, cov, sample_Z_c)
+
+    assert_array_almost_equal(expected, row_x, 0.01)
