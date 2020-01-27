bede0465a0834379747b9d5aaf451ea75e40e5f7
tobiasstenzel <tobias.stenzel@mailbox.org>
2020-01-27 22:44:44 +0100

    sampling for EEind is correct

--- python/sandbox.py
+++ python/sandbox.py
@@ -34,7 +34,7 @@ cov = np.array(
 n_inputs=5
 n_rows = n_inputs + 1
 n_levels=6
-n_traj_sample = 10
+n_traj_sample = 1
 sample_traj_list = list()
 for traj in range(0, n_traj_sample):
     seed = 123 + traj
@@ -43,12 +43,10 @@ for traj in range(0, n_traj_sample):
 
 zero_idx_diff = []
 one_idx_diff = []
-two_idx_diff = []
 
 for traj in range(0, n_traj_sample):
     zero_idx_diff.append(ee_ind_reorder_trajectory(sample_traj_list[traj], p_i_plus_one=False))
     one_idx_diff.append(ee_ind_reorder_trajectory(sample_traj_list[traj]))
-    two_idx_diff.append(ee_full_reorder_trajectory(sample_traj_list[traj]))
 
 
 mu_zero = reorder_mu(mu)
@@ -56,7 +54,7 @@ cov_zero = reorder_cov(cov)
 for traj in range(0, n_traj_sample):
     for row in range(0, n_rows):
         zero_idx_diff[traj][row, :] = transform_stnormal_normal_corr_lemaire09(
-                one_idx_diff[traj][row, :], cov_zero, mu_zero)
+                zero_idx_diff[traj][row, :], cov_zero, mu_zero)
         mu_zero = reorder_mu(mu_zero)
         cov_zero = reorder_cov(cov_zero)
 
@@ -70,23 +68,11 @@ for traj in range(0, n_traj_sample):
         mu_one = reorder_mu(mu_one)
         cov_one = reorder_cov(cov_one)
 
-
-mu_two = inverse_reorder_mu(mu)
-cov_two = inverse_reorder_cov(cov)
-for traj in range(0, n_traj_sample):
-    for row in range(0, n_rows):
-        two_idx_diff[traj][row, :] = transform_stnormal_normal_corr_lemaire09(
-                one_idx_diff[traj][row, :], cov_two, mu_two)
-        mu_two = reorder_mu(mu_two)
-        cov_two = reorder_cov(cov_two)
-
 trans_zero = []
 trans_one = []
-trans_two = []
 for traj in range(0, n_traj_sample):
     trans_zero.append(inverse_ee_ind_reorder_trajectory(zero_idx_diff[traj], p_i_plus_one=False))
     trans_one.append(inverse_ee_ind_reorder_trajectory(one_idx_diff[traj]))
-    trans_two.append(inverse_ee_full_reorder_trajectory(two_idx_diff[traj]))
 
 
 
