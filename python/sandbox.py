b5fde363575eb0f84d103a7ff2e48219005daf06
tobiasstenzel <tobias.stenzel@mailbox.org>
2020-01-27 20:36:06 +0100

    pre-correction hopefully

--- python/sandbox.py
+++ python/sandbox.py
@@ -1,16 +1,39 @@
 """Sandbox."""
+
+import numpy as np
+
+
 from sampling_trajectory import morris_trajectory
+
 from transform_reorder import ee_ind_reorder_trajectory
 from transform_reorder import ee_full_reorder_trajectory
 from transform_reorder import inverse_ee_ind_reorder_trajectory
 from transform_reorder import inverse_ee_full_reorder_trajectory
+from transform_reorder import reorder_mu
+from transform_reorder import reorder_cov
+from transform_reorder import inverse_reorder_mu
+from transform_reorder import inverse_reorder_cov
+
+from transform_distributions import transform_stnormal_normal_corr_lemaire09
+
+mu = np.array([10, 11, 12, 13, 14])
+
+# Covariance matrix.
+cov = np.array(
+    [
+        [10, 0, 0, 2, 0.5],
+        [0, 20, 0.4, 0.15, 0],
+        [0, 0.4, 30, 0.05, 0],
+        [2, 0.15, 0.05, 40, 0],
+        [0.5, 0, 0, 0, 50],
+    ]
+)
 
 
 
-
-
-
-
+n_inputs=5
+n_rows = n_inputs + 1
+n_levels=6
 n_traj_sample = 10
 sample_traj_list = list()
 for traj in range(0, n_traj_sample):
@@ -26,6 +49,49 @@ for traj in range(0, n_traj_sample):
     zero_idx_diff.append(ee_ind_reorder_trajectory(sample_traj_list[traj], p_i_plus_one=False))
     one_idx_diff.append(ee_ind_reorder_trajectory(sample_traj_list[traj]))
     two_idx_diff.append(ee_full_reorder_trajectory(sample_traj_list[traj]))
+
+
+mu_zero = reorder_mu(mu)
+cov_zero = reorder_cov(cov)
+for traj in range(0, n_traj_sample):
+    for row in range(0, n_rows):
+        zero_idx_diff[traj][row, :] = transform_stnormal_normal_corr_lemaire09(
+                one_idx_diff[traj][row, :], cov_zero, mu_zero)
+        mu_zero = reorder_mu(mu_zero)
+        cov_zero = reorder_cov(cov_zero)
+
+
+mu_one = mu
+cov_one = cov
+for traj in range(0, n_traj_sample):
+    for row in range(0, n_rows):
+        one_idx_diff[traj][row, :] = transform_stnormal_normal_corr_lemaire09(
+                one_idx_diff[traj][row, :], cov_one, mu_one)
+        mu_one = reorder_mu(mu_one)
+        cov_one = reorder_cov(cov_one)
+
+
+mu_two = inverse_reorder_mu(mu)
+cov_two = inverse_reorder_cov(cov)
+for traj in range(0, n_traj_sample):
+    for row in range(0, n_rows):
+        two_idx_diff[traj][row, :] = transform_stnormal_normal_corr_lemaire09(
+                one_idx_diff[traj][row, :], cov_two, mu_two)
+        mu_two = reorder_mu(mu_two)
+        cov_two = reorder_cov(cov_two)
+
+trans_zero = []
+trans_one = []
+trans_two = []
+for traj in range(0, n_traj_sample):
+    trans_zero.append(inverse_ee_ind_reorder_trajectory(zero_idx_diff[traj], p_i_plus_one=False))
+    trans_one.append(inverse_ee_ind_reorder_trajectory(one_idx_diff[traj]))
+    trans_two.append(inverse_ee_full_reorder_trajectory(two_idx_diff[traj]))
+
+
+
+
+        
 
 
 
