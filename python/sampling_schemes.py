implement large integration test, EE works but wrong order

python/sampling_schemes.py
@@ -127,7 +127,7 @@ def morris_trajectory(
     """
     np.random.seed(seed)
 
-    step = step_function(n_levels)
+    step = stepsize(n_levels)
 
     #  Assisting matrices to induce stairs shape; Lower triangular matrix of ones.
     B = np.tril(np.ones([n_inputs + 1, n_inputs]), -1)

python/test.py
@@ -63,7 +63,7 @@ steps_two = np.array([2/3, 2/3, 2/3, -2/3, -2/3, -2/3])
 def sobol_model_three(d, a, e, b, c, f, *args):
     """Reordered because trajectory in primer shuffled the trajectory columns"""
     input_pars = np.array([d, a, e, b, c, f])
-    coeffs = np.array([2, 78, 97, 12, 0.4, 33])
+    coeffs = np.array([2, 78, 97, 12, 0.5, 33])
 
     def g_i(input_pars, coeffs):
         return (abs(4 * input_pars - 2) + coeffs) / (1 + coeffs)
@@ -113,6 +113,9 @@ traj_one = np.array([[0, 2/3, 1, 0, 0, 1/3],
                      [2/3, 0, 1, 2/3, 0, 1],
                      [2/3, 0, 1/3, 2/3, 0, 1],
                      [2/3, 0, 1/3, 2/3, 2/3, 1]])
+    
+idx_one = [5,1,0,3,2,4]
+traj_one = traj_one[:, idx_one]
 
 traj_two = np.array([[0, 1/3, 1/3, 1, 1, 2/3],
                     [0, 1, 1/3, 1, 1, 2/3],
@@ -122,6 +125,11 @@ traj_two = np.array([[0, 1/3, 1/3, 1, 1, 2/3],
                     [2/3, 1, 1, 1, 1/3, 0],
                     [2/3, 1, 1, 1/3, 1/3, 0]])
 
+idx_two = [1,2,0,5,4,3]
+traj_two = traj_two[:, idx_two]    
+    
+    
+    
 traj_three = np.array([[1, 2/3, 0, 2/3, 1, 0],
                     [1, 2/3, 0, 0, 1, 0],
                     [1/3, 2/3, 0, 0, 1, 0],
@@ -130,6 +138,10 @@ traj_three = np.array([[1, 2/3, 0, 2/3, 1, 0],
                     [1/3, 0, 2/3, 0, 1/3, 0],
                     [1/3, 0, 2/3, 0, 1/3, 2/3]])
 
+idx_three = [3,0,4,1,2,5]
+traj_three = traj_three[:, idx_three]    
+    
+    
 traj_four = np.array([[1, 1/3, 2/3, 1, 0, 1/3],
                     [1, 1/3, 2/3, 1, 0, 1],
                     [1, 1/3, 0, 1, 0, 1],
@@ -139,7 +151,8 @@ traj_four = np.array([[1, 1/3, 2/3, 1, 0, 1/3],
                     [1/3, 1, 0, 1/3, 2/3, 1]])
 
 
-
+idx_four = [5,2,3,4,1,0]
+traj_four = traj_four[:, idx_four]   
 
 (
     one_ee_ind,
@@ -213,13 +226,13 @@ traj_four = np.array([[1, 1/3, 2/3, 1, 0, 1/3],
 )
 
 
-ee_one = np.array([one_ee_ind[5], one_ee_ind[1], one_ee_ind[0], one_ee_ind[3], one_ee_ind[2], one_ee_ind[4]]).reshape(6,1)
+ee_one = np.array(one_ee_ind).reshape(6,1)
 
-ee_two = np.array([two_ee_ind[1], two_ee_ind[2], two_ee_ind[0], two_ee_ind[5], two_ee_ind[4], two_ee_ind[3]]).reshape(6,1)
+ee_two = np.array([two_ee_ind[1], two_ee_ind[2], two_ee_ind[0], two_ee_ind[5], two_ee_ind[4], two_ee_ind[3]]).reshape(6,1)[idx_two]
 
-ee_three = np.array([three_ee_ind[1], three_ee_ind[3], three_ee_ind[4], three_ee_ind[0], three_ee_ind[2], three_ee_ind[5]]).reshape(6,1)
+ee_three = np.array([three_ee_ind[1], three_ee_ind[3], three_ee_ind[4], three_ee_ind[0], three_ee_ind[2], three_ee_ind[5]]).reshape(6,1)[idx_three]
 
-ee_four = np.array([four_ee_ind[5], four_ee_ind[4], four_ee_ind[1], four_ee_ind[2], four_ee_ind[3], four_ee_ind[0]]).reshape(6,1)
+ee_four = np.array([four_ee_ind[5], four_ee_ind[4], four_ee_ind[1], four_ee_ind[2], four_ee_ind[3], four_ee_ind[0]]).reshape(6,1)[idx_four]
 
 ee_i = np.concatenate((ee_one, ee_two, ee_three, ee_four), axis=1)
 
@@ -232,6 +245,19 @@ ee = np.mean(ee_i, axis=1).reshape(6,1)
 ee_abs = np.mean(abs(ee_i), axis=1).reshape(6,1)
 
 ee_sd = np.sqrt(np.var(ee_i, axis=1)).reshape(6,1)
+
+
+
+for i in range(0,7):
+    print(sobol_model_four(*traj_four[i, :]))
+
+
+
+coeffs = np.array([78, 12, 0.5, 2, 97, 33])
+
+#coeffs_one = np.array([33, 12, 78, 2, 0.5, 97])
+#coeffs_two = coeffs[idx_two]
+
 
 
 