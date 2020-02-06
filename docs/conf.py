6aeab967870e154b17da9ce97ac549564dbcb85c
tobiasstenzel <tobias.stenzel@mailbox.org>
2020-02-07 00:05:28 +0100

    fix rtd?

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ import os
 import sys
 
 # Adjust path to online Readthedocs. No package, therefore more dots.
-sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../.."))
 
 # -- Project information -----------------------------------------------------
 
@@ -67,7 +67,9 @@ html_theme = "alabaster"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+
+# deleted "_static"
+html_static_path = []
 
 
 # -- Extension configuration -------------------------------------------------
