345c408a43e081399937fe49693e2d10549b8234
tobiasstenzel <tobias.stenzel@mailbox.org>
2020-02-07 01:22:30 +0100

    fix rtd?

--- docs/conf.py
+++ docs/conf.py
@@ -40,6 +40,10 @@ extensions = [
     "numpydoc",
 ]
 
+# try to fix TypeError: 'rcParams' object does not support item assignment
+# following https://github.com/readthedocs/readthedocs.org/issues/1803
+# import matplotlib.pyplot 
+
 # Proposed here to fix library import errros that depend on C
 # http://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#confval-autodoc_mock_imports
 autodoc_mock_imports = ["numpy", "matplotlib", "pandas", "chaospy", "respy", "scipy", "seaborn", "estimagic"]
