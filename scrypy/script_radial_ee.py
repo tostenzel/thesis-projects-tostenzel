5146d4536c76b1b0198714a7a6a60495a4c9bdb0
tobiasstenzel <tobias.stenzel@mailbox.org>
2020-02-13 07:10:35 +0100

    completed radial computation

--- scrypy/script_radial_ee.py
+++ scrypy/script_radial_ee.py
@@ -1,11 +1,9 @@
 """From Radial samples, compute EE-based measures for single QoI from KW94."""
-
 import pickle
-import pandas as pd
 import time
 
-
-from multi_quantities_of_interest import  multi_quantities_of_interest
+import pandas as pd
+from multi_quantities_of_interest import multi_quantities_of_interest
 from sampling_schemes import radial_sample
 from screening_measures import screening_measures
 
@@ -14,6 +12,7 @@ params = pd.read_pickle("input/est_rand_params_chol.pkl")
 mean = params["value"].to_numpy()
 cov = pd.read_pickle("input/est_cov_chol.pkl").to_numpy()
 
+
 def wrapper_qoi(*args):
     """
     Transform different single arguments for `screening_measures` to a list for
@@ -21,7 +20,7 @@ def wrapper_qoi(*args):
     Also, return only the first main qoi.
     
     """
-    
+
     args_list = [args]
     return multi_quantities_of_interest(*args_list)[0]
 
@@ -29,39 +28,41 @@ def wrapper_qoi(*args):
 n_inputs = len(mean)
 
 # Rad-exclusive params.
-n_sample_rad = 2
+n_sample_rad = 100
 
 
-#traj_list, step_list_traj = trajectory_sample(n_sample_traj, n_inputs, n_levels, seed, normal=True, numeric_zero)
+# traj_list, step_list_traj = trajectory_sample(n_sample_traj, n_inputs, n_levels, seed, normal=True, numeric_zero)
 rad_list, step_list_rad = radial_sample(n_sample_rad, n_inputs, normal=True)
 
 # Save measures and steps to results dir.
-with open('input/sample_radial.pkl', 'wb') as f:
-  pickle.dump(rad_list, f)
+with open("input/sample_radial.pkl", "wb") as f:
+    pickle.dump(rad_list, f)
 
-with open('input/steps_radial.pkl', 'wb') as f:
-  pickle.dump(step_list_rad, f)
+with open("input/steps_radial.pkl", "wb") as f:
+    pickle.dump(step_list_rad, f)
 
 
 start = time.time()
 
-measures_list_rad, obs_list_rad = screening_measures(wrapper_qoi, rad_list, step_list_rad, cov, mean, radial = True)
+measures_list_rad, obs_list_rad = screening_measures(
+    wrapper_qoi, rad_list, step_list_rad, cov, mean, radial=True
+)
 
 end = time.time()
 print(end - start)
-comp_time= (end - start)/60
+comp_time = (end - start) / 60
 
 
 # Save measures and steps to results dir.
-with open('results/measures_radial.pkl', 'wb') as f:
-  pickle.dump(measures_list_rad, f)
+with open("results/measures_radial.pkl", "wb") as f:
+    pickle.dump(measures_list_rad, f)
 
-with open('results/ee_obs_radial.pkl', 'wb') as f:
-  pickle.dump(obs_list_rad, f)
+with open("results/ee_obs_radial.pkl", "wb") as f:
+    pickle.dump(obs_list_rad, f)
 
 # Load to check.
-with open('results/measures_radial.pkl', 'rb') as f:
-  read_meas = pickle.load(f)
-  
-with open('results/ee_obs_radial.pkl', 'rb') as f:
-  read_ee_obs = pickle.load(f)
+with open("results/measures_radial.pkl", "rb") as f:
+    read_meas = pickle.load(f)
+
+with open("results/ee_obs_radial.pkl", "rb") as f:
+    read_ee_obs = pickle.load(f)
